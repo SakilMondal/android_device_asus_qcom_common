e8051919e2e41feeb516075ef4065c4cf2dc0eb7
theimpulson <aayushgupta219@gmail.com>
2018-11-21 17:08:16 +0530

    msm8916-common: Add camera provider service

--- msm8916.mk
+++ msm8916.mk
@@ -129,6 +129,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+	android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     bspcapability \
     camera.msm8916 \
