c9bf89038012ba969f22e4c722cee25b5759bc5b
SakilMondal <mondalsakil@gmail.com>
2018-12-11 17:46:52 +0530

    Revert "msm8916-common: Add camera provider service"
    This reverts commit e8051919e2e41feeb516075ef4065c4cf2dc0eb7.

--- msm8916.mk
+++ msm8916.mk
@@ -130,7 +130,6 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-	android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     bspcapability \
     camera.msm8916 \
