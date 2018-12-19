3d231284859188046ab7b9f9c0ff146523c88b8e
mikeNG <mkbestas@lineageos.org>
2018-12-19 22:25:26 +0530

    msm8916-common: Remove dexpreopt configuration
    * It's set globally now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -89,16 +89,6 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
-# Dex
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
-
 # Display
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
 MAX_EGL_CACHE_SIZE := 2048*1024
