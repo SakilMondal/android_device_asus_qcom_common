44eb061a6f10b1c18eaab56cbd1b6c0ea1232570
SakilMondal <mondalsakil@gmail.com>
2019-05-04 16:48:48 +0000

    msm8916-common: AOSP-ify
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -16,9 +16,8 @@
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
+    $(LOCAL_PATH)/overlay
+	
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
